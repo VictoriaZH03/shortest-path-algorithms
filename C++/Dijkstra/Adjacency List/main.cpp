a5504bd955e66c18870fa64e11a07dc0086f8e4d
ViktoriyaZHristova <123359088+VictoriaZH03@users.noreply.github.com>
2024-03-02 23:05:22 +0200

    Update main.cpp

--- C++/Dijkstra/Adjacency List/main.cpp
+++ C++/Dijkstra/Adjacency List/main.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+#include "dijkstra.h"
+
+int main()
+{
+	adjList = {
+	{{1, 4}, {2, 1}},	// Node 0: (1,4), (2, 1)
+	{{3, 2}},			// Node 1: (3, 2)
+	{{1, 5}, {3, 1}},	// Node 2: (1, 5), (3, 1)
+	{{4, 3}}};			// Node 3: (4, 3)
+	
+	// source node for Dijkstra's algorithm
+	int src = 3;
+	
+	// test the algorithm
+	vector<int> shortestDistances = dijkstra(src, adjList.size());
+	
+	//output shortest distances
+	for (int i = 0; i < shortestDistances.size(); ++i)
+	{
+		cout << "Shortest distance from node " << src << " to node " << i << " is: ";
+		if (shortestDistances[i] == INF)
+		{
+			cout << "INF" << endl;
+		}
+		else
+		{
+			cout << shortestDistances[i] << endl;
+		}
+	}
+	
+	
+	return 0;
+}//main
