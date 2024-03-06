6e2298e19acb4b55bf2ef81117e5117ef694e830
ViktoriyaZHristova <123359088+VictoriaZH03@users.noreply.github.com>
2024-03-06 10:52:32 +0200

    Update main.cpp

--- C++/A*/main.cpp
+++ C++/A*/main.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+#include "astar.h"
+
+int main()
+{
+	// Example graph initialization
+	vector<vector<Edge>> graph =
+	{
+		{{1, 4}, {2, 1}},	// Node 0
+		{{3, 2}},			// Node 1
+		{{1, 5}, {3, 1}},	// Node 2
+		{{4, 3}},			// Node 3
+		{},					// Node 4 (target)
+    };
+
+	int source = 2;
+	int target = 4;
+
+	// Find shortest path using A* algorithm
+	vector<int> shortest_path = astar(graph, source, target);
+
+	// Print shortest path
+	cout << "Shortest Path from " << source << " to " << target << ": ";
+	for (int node : shortest_path)
+	{
+		cout << node << " ";
+	}
+	cout << endl;
+
+
+	return 0;
+}
