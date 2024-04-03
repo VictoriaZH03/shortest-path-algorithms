3403333a88ca8b4970009aeddc953f7d9560be42
ViktoriyaZHristova <123359088+VictoriaZH03@users.noreply.github.com>
2024-04-03 17:45:34 +0300

    Update dijkstra.h

--- C++/Dijkstra/Adjacency List/dijkstra.h
+++ C++/Dijkstra/Adjacency List/dijkstra.h
@@ -1,4 +1,3 @@
-
 // IMPLEMENTATION USING ADJACENCY_LIST
 
 
@@ -17,49 +16,42 @@ typedef pair<int, int> ii;
 vector<vector<ii>> adjList;
 
 
-// Dijkstra's algorithm implementation
-vector<int> dijkstra(int src, int n)
+// Dijkstra algorithm
+vector<int> dijkstra(int src, const vector<vector<ii>>& adjList)
 {
-	// create a vector to store the shortest distances from the source vertex to all other vertices, initialize all distances with infinity
+	int n = adjList.size();
 	vector<int> shortestDistances(n, INF);
-	// create a priority queue to store vertices and their distances from the source, here a min-heap is used so the pair with the smallest distance will be at the top
 	priority_queue<ii, vector<ii>, greater<ii>> pq;
-	
-	// set the distance of the source vertex to itself to 0
+
 	shortestDistances[src] = 0;
-	// push the source vertex and its distance (0) into priority queue
 	pq.push({0, src});
-	
-	// main loop of Dijkstra's algorithm
+
 	while (!pq.empty())
 	{
-		// extract the vertex with the smallest distance
-		int curr_vertex = pq.top().second;	// get the vertex
-		int curr_dist = pq.top().first;	// get the distance
-		pq.pop();									// remove the vertex from pq
-		
-		
-		// if the extracted distance is greater than the distance already calculated for this vertex, it means we've found a shorter path in the past, so we skip this vertex
-		if (curr_dist > shortestDistances[curr_vertex]) continue;
-		
-		// iterato through all the adjacent vertices of curr_vertex
+		int curr_vertex = pq.top().second;
+		int curr_dist = pq.top().first;
+		pq.pop();
+
+		if (curr_dist > shortestDistances[curr_vertex])
+		{
+			continue;
+		}
+
 		for (auto edge : adjList[curr_vertex])
 		{
-			int neighbor = edge.first;	// adjacent vertex
-			int weight = edge.second;	// weight of the edge from curr_vertex to neighbor
-			
-			// if the distance from source to curr_vertex plus the weight of the edge from curr_vertex to neighbor is less than the current distance from source to neighbor, update the distance
+			int neighbor = edge.first;
+			int weight = edge.second;
+
 			if (shortestDistances[curr_vertex] + weight < shortestDistances[neighbor])
 			{
-				// udpate the distance to neighbor
 				shortestDistances[neighbor] = shortestDistances[curr_vertex] + weight;
-				// push the updated distance and neighbor into pq
 				pq.push({shortestDistances[neighbor], neighbor});
 			}
-		}
+	 	}
 	}
-	// return the vector containing the shortest distance from source vertex
+
 	return shortestDistances;
 }
+
 
 #endif
