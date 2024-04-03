75c0af67cbc445841fc253b8c8e0ce284c17ab30
ViktoriyaZHristova <123359088+VictoriaZH03@users.noreply.github.com>
2024-04-03 17:47:21 +0300

    Update main.cpp

--- C++/Dijkstra/Adjacency Matrix/main.cpp
+++ C++/Dijkstra/Adjacency Matrix/main.cpp
@@ -1,49 +1,65 @@
-#include <bits/stdc++.h>
+// IMPLEMENTATION USING ADJACENCY_MATRIX
+
+#include <iostream>
+#include <fstream>
+#include "dijkstra.h"
 
 using namespace std;
 
-#include "dijkstra.h"
-
 int main()
 {
-	// example adjacency matrix representing a graph
-	int V = 5;	// number of vertices
-	int** graph = new int*[V];
-	
-	for (int i = 0; i < V; ++i)
-	{
-		graph[i] = new int[V];
-		for (int j = 0; j < V; ++j)
-		{
-			// initialize with INF except for the diagonal
-			graph[i][j] = (i == j) ? 0 : INF;
-		}
-	}
-	
-	// example edges of graph
-	graph[0][1] = 4;
-	graph[0][2] = 1;
-	graph[1][3] = 2;
-	graph[2][1] = 5;
-	graph[2][3] = 1;
-	graph[3][4] = 3;
-	
-	// source vertex
-	int src = 0;
-	
-	int* shortestDistances = dijkstra(graph, V, src);
-	
-	// output shortest distances
-	cout << "Shortest distances from vertex " << src << " to all other vertices:" << endl;
-	for (int i = 0; i < V; ++i)
-	{
-		cout << "Vertex " << i << ": " << shortestDistances[i] << endl;
-	}
-	
-	// deallocate memory
-	deallocateMatrix(graph, V);
-	delete[] shortestDistances;
-	
-	
-	return 0;
+    // number of vertices
+    const int V = 5;
+
+    // initialize the graph adjacency matrix with INF
+    int** graph = new int*[V];
+    for (int i = 0; i < V; i++)
+    {
+        graph[i] = new int[V];
+        for (int j = 0; j < V; j++)
+        {
+            graph[i][j] = INF;
+        }
+    }
+
+    // read the graph information from the file and populate the adjacency matrix
+    ifstream inputFile("graph.txt");
+    if (!inputFile)
+    {
+        cerr << "Error: Unable to open the file." << endl;
+        return 1;
+    }
+
+    int node, neighbor, weight;
+    while (inputFile >> node >> neighbor >> weight)
+    {
+        // add the edge to the adjacency matrix
+        graph[node][neighbor] = weight;
+    }
+    inputFile.close();
+
+    // source vertex
+    int src = 0; 
+
+    // run Dijkstra's algorithm
+    int* shortestDistances = dijkstra(graph, V, src);
+
+    // output shortest distances from source vertex
+    cout << "Shortest distances from vertex " << src << " to all other vertices:" << endl;
+    for (int i = 0; i < V; i++)
+    {
+        cout << "Vertex " << i << ": " << shortestDistances[i] << endl;
+    }
+
+    // deallocate memory for the adjacency matrix
+    for (int i = 0; i < V; i++)
+    {
+        delete[] graph[i];
+    }
+    delete[] graph;
+
+    // Deallocate memory for the shortest distances array
+    delete[] shortestDistances;
+
+    return 0;
 }//main
